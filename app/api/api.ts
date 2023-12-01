3f383fc22bcd5163f4b596606ece678a01b1d577
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-12-01 15:38:38 +0900

    add axios

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,11 +1,22 @@
+import axios from 'axios';
 import { User, Post } from '../types';
 
 export async function fetchUsers(): Promise<User[]> {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    return response.json();
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
+  }
 }
-  
+
 export async function fetchPostsByUserId(userId: number): Promise<Post[]> {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-    return response.json();
+  try {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching posts for userId ${userId}:`, error);
+    throw error;
+  }
 }
